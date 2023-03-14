78506bef5ecbd1214d5655226da79f80973c6ba0
Daniel Hochegger <d.lehr@htlkrems.at>
2023-03-14 19:11:45 +0100

    finished

---
+++ MessomatVisualizer/View/Data/MeasureResult.cs
@@ -0,0 +1,6 @@
+﻿namespace View.Data; 
+
+public class MeasureResult {
+    public int Luminosity { get; set; }
+    public bool DigitalStatus { get; set; }
+}
---
+++ MessomatVisualizer/View/Data/SerialPortService.cs
@@ -0,0 +1,66 @@
+﻿using System.IO.Ports;
+
+namespace View.Data;
+
+public class SerialPortService {
+    private SerialPort mySerialPort;
+    public MeasureResult SerialData { get; set; } = new();
+    public string RawData = "";
+
+
+    public SerialPortService() {
+        mySerialPort = new SerialPort("COM3");
+        mySerialPort.BaudRate = 9600;
+
+        mySerialPort.NewLine = (@"&N");
+
+        if (!mySerialPort.IsOpen) {
+            mySerialPort.DataReceived += (DataReceivedHandler);
+        }
+    }
+
+    private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e) {
+        SerialPort sp = (SerialPort) sender;
+        try {
+            string indata = sp.ReadExisting();
+            if (indata.Contains((char) 21))
+                return;
+            if (indata.Contains((char) 0))
+                return;
+
+            RawData += indata;
+            //Console.WriteLine(RawData);
+            var split = String.Empty;
+            split += (char) 3;
+            var dataSplit = RawData.Split(split);
+
+            if (dataSplit.Length < 2) {
+                return;
+            }
+
+            foreach (var data in (dataSplit.Where(d => d.Contains("A")))) {
+                foreach (var s in data.Split(";")) {
+                    if (s.Split(":")[0].Contains("LV")) {
+                        Console.WriteLine(s.Split(":")[1]);
+                        SerialData.Luminosity = Convert.ToInt32(s.Split(":")[1]);
+                    }
+                    if (s.Split(":")[0].Contains("BV")) {
+                        Console.WriteLine(s.Split(":")[1]);
+                        SerialData.Luminosity = Convert.ToInt32(s.Split(":")[1]);
+                    }
+                }
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine("Error: {0}", ex.Message);
+        }
+    }
+
+    public Task<MeasureResult> GetSerialValue() {
+        return Task.FromResult(SerialData);
+    }
+
+    public void Write(string cmd) {
+        mySerialPort.Write("<" + cmd + ">");
+    }
+}
